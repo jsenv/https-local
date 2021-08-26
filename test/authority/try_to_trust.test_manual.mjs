68e251203459a525a02fc5e7033469dd7ea2c1c0
Damien Maillard <damien.maillard@dailymotion.com>
2021-08-26 06:18:33 +0200

    reorg test file

--- test/authority/try_to_trust.test_manual.mjs
+++ test/authority/try_to_trust.test_manual.mjs
@@ -5,16 +5,18 @@ import {
 } from "@jsenv/https-localhost"
 import { startServerForTest } from "@jsenv/https-localhost/test/test_helpers.mjs"
 
-await uninstallCertificateAuthority()
+await uninstallCertificateAuthority({
+  tryToUntrust: true,
+})
 await installCertificateAuthority({
   tryToTrust: true,
 })
-const { serverCertificate, serverPrivateKey } = await requestCertificateForLocalhost()
+const { serverCertificate, serverCertificatePrivateKey } = await requestCertificateForLocalhost()
 
 const serverOrigin = await startServerForTest({
   port: 4456,
   serverCertificate,
-  serverPrivateKey,
+  serverCertificatePrivateKey,
   keepAlive: true,
 })
 console.log(`Open ${serverOrigin} in a browser`)

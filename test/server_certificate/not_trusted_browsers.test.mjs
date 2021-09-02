ba7b3ee84fbe61f6baf7fbfc6651648abcdd2a00
Damien Maillard <damien.maillard@dailymotion.com>
2021-09-02 09:01:10 +0200

    update expected for windows

--- test/server_certificate/not_trusted_browsers.test.mjs
+++ test/server_certificate/not_trusted_browsers.test.mjs
@@ -55,7 +55,7 @@ const serverOrigin = await startServerForTest({
     throw new Error("should throw")
   } catch (e) {
     const actual = e.errorText
-    const expected = "SEC_ERROR_UNKNOWN_ISSUER"
+    const expected = process.platform === "win32" ? undefined : "SEC_ERROR_UNKNOWN_ISSUER"
     assert({ actual, expected })
   } finally {
     browser.close()
